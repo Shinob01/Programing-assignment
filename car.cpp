886c897d0e0d6e08a60db8ae8eeff11ed8434186
Shinobu <0heartunderblade0@gmail.com>
2022-12-22 21:28:43 +0500

    first commit

---
+++ car.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+main()
+{
+system("color 09");
+
+cout << "           #######################################          " << endl;
+cout << "           #                  #                  #          " << endl;
+cout << "           #                  #                  #          " << endl;
+cout << "           #                  #                  #          " << endl;
+cout << " ########################################################## " << endl;
+cout << " #                     ()     #    ()                     # " << endl;
+cout << " #                            #                           # " << endl;
+cout << " #        #####               #               #####       # " << endl;
+cout << " #      ##     ##             #             ##     ##     # " << endl;
+cout << " ########################################################## " << endl;
+cout << "       ##       ##                        ##       ##       " << endl;
+cout << "        ##     ##                          ##     ##        " << endl;
+cout << "          #####                              #####          " << endl;
+}
---
+++ colorcow.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+main()
+{
+system("color 09");
+
+cout << "    -------------------------  " << endl;
+cout << "       o   ^__^                " << endl;
+cout << "        o  (oo)\\_______       " << endl;
+cout << "           (__)\\       )\\/\\ " << endl;
+cout << "               ||----W |       " << endl;
+cout << "               ||     ||       " << endl;
+cout << "              ---    ---       " << endl;
+}
